1a60d025ad76e0fffb328fb0fec32a62e597faf8
eunbime <ebk1014@gmail.com>
2024-05-12 19:11:47 +0900

    feat: 로그인 모달 react-hook-form 적용

--- src/components/modal/login-modal/LoginModal.tsx
+++ src/components/modal/login-modal/LoginModal.tsx
@@ -1,11 +1,34 @@
+'use client';
+
 import React from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Modal from '../Modal';
 
 function LoginModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <Modal>
       <h2 className="font-bold text-3xl text-center my-12">로그인</h2>
-      <section className="flex flex-col items-center gap-y-4 max-w-sm mx-auto w-full">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center gap-y-4 max-w-sm mx-auto w-full"
+      >
         <div className="grid gap-y-1.5 w-full ">
           <label htmlFor="email" className="text-sm font-medium text-gray-800">
             이메일
@@ -14,6 +37,7 @@ function LoginModal() {
             id="email"
             type="email"
             className="block border w-full px-6 py-3 rounded focus:border-black outline-none transition border-slate-300"
+            {...register('email', { required: true })}
           />
         </div>
         <div className="grid gap-y-1.5 w-full">
@@ -27,15 +51,22 @@ function LoginModal() {
             id="password"
             type="password"
             className="block border w-full px-6 py-3 rounded focus:border-black outline-none transition border-slate-300"
+            {...register('password', { required: true })}
           />
         </div>
+        {(errors.email || errors.password) && (
+          <span className="text-red-500">
+            이메일 또는 비밀번호가 일치하지 않습니다.
+          </span>
+        )}
         <button
-          className="mt-2 border border-slate-700 py-4 px-12 text-[15px] font-semibold bg-white transition hover:-translate-y-1 active:translate-y-0 hover:drop-shadow w-full data-[color=black]:bg-black data-[color=black]:text-white "
+          type="submit"
+          className="mt-3 border border-slate-700 py-4 px-12 text-[15px] font-semibold bg-white transition hover:-translate-y-1 active:translate-y-0 hover:drop-shadow w-full data-[color=black]:bg-black data-[color=black]:text-white "
           data-color="black"
         >
           로그인하기
         </button>
-      </section>
+      </form>
     </Modal>
   );
 }
